ee309fb1739233fe1b3b2e082aa5141c4bc50a84
SaiKathika <43703634+SaiKathika@users.noreply.github.com>
2020-04-09 19:35:24 +0530

    Create Coinbase_Price_fetcher.py

---
+++ Coinbase_Price_fetcher.py
@@ -0,0 +1,42 @@
+from notify_run import Notify   #To get notification on Mobile
+import requests
+import json
+import time
+from win10toast import ToastNotifier    #To get notification on Windows 10
+
+notify = Notify()
+
+toaster = ToastNotifier()
+
+class Price:
+    
+    def display(self):
+        data = response.json()
+        #This line gets the Cryptocurrency name
+        crypto = data["data"]["base"]
+        #This line gets the Price
+        price = data["data"]["amount"]
+        return price
+
+while True:
+    print("Ethereum")
+    price = Price()
+    
+    response = requests.get("https://api.coinbase.com/v2/prices/ETH-INR/buy")
+    #This line is used to display on terminal
+    print(f"Buy_Price: {price.display()}")
+
+    #This line is used to send notification to Mobile
+    notify.send(f"Buy_Price: {price.display()}")     
+    
+    #This line is used to send notification to Windows 10 Screen
+    toaster.show_toast(f"Buy_Price: {price.display()}")   
+
+    response = requests.get("https://api.coinbase.com/v2/prices/ETH-INR/sell")
+    print(f"Sell_Price: {price.display()}")
+    
+    response = requests.get("https://api.coinbase.com/v2/prices/ETH-INR/spot")
+    print(f"Spot_Price: {price.display()}")
+
+    #sleep function will trigger for every 2 Minutes
+    time.sleep(120)
